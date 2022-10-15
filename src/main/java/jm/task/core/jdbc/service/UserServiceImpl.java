8b6a8bd970aca8625b428642b9722060c8020457
Cilez <kobrelislegryi@gmail.com>
2022-10-15 11:44:55 +0300

    Что-то пошло не так, но вроде исправил.

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,13 +1,12 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
-
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private UserDaoJDBCImpl dao = new UserDaoJDBCImpl();
+    private final UserDao dao = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
         dao.createUsersTable();
